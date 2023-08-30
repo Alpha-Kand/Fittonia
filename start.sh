63043ec30f94b79a3aeaaacdec777c4a5aaea0aa
Hunter Wiesman <alphakand@gmail.com>
2023-08-30 10:43:37 -0600

    Updated convenience app running scripts.

--- start.sh
+++ start.sh
@@ -1,2 +1,3 @@
-java -jar ./build/compose/jars/FittoniaTerminal-linux-x64-1.0.jar server &
-java -jar ./build/compose/jars/FittoniaTerminal-linux-x64-1.0.jar client &
+#!/bin/bash
+
+java -jar ./build/compose/jars/FittoniaTerminal-linux-x64-1.0.jar $@
