cba9bbbe163c115432575aa4a4d4804551553931
Hunter Wiesman <alphakand@gmail.com>
2023-09-20 13:33:11 -0600

    Create Client Engine sub-program.

--- src/desktopMain/kotlin/main.clientengine.kt
+++ src/desktopMain/kotlin/main.clientengine.kt
@@ -1,1 +1,13 @@
-fun main(args: Array<String>) {}
+import commandHandler.CommandHandler
+import commandHandler.SendFilesCommand
+import commandHandler.SendMessageCommand
+import commandHandler.executeCommand.sendFilesExecution
+import commandHandler.executeCommand.sendMessageExecution
+
+fun main(args: Array<String>) {
+    when (val command = CommandHandler(args = args).getCommand()) {
+        is SendFilesCommand -> sendFilesExecution(command = command)
+        is SendMessageCommand -> sendMessageExecution(command = command)
+        else -> Unit
+    }
+}
--- src/desktopMain/kotlin/main.terminal.kt
+++ src/desktopMain/kotlin/main.terminal.kt
@@ -12,13 +12,13 @@ import commandHandler.executeCommand.addExecution
 import commandHandler.executeCommand.dumpExecution
 import commandHandler.executeCommand.listDestinationsExecution
 import commandHandler.executeCommand.removeExecution
-import commandHandler.executeCommand.sendFilesExecution
-import commandHandler.executeCommand.sendMessageExecution
 import commandHandler.executeCommand.serverExecution
 import commandHandler.executeCommand.serverPasswordExecution
 import commandHandler.executeCommand.setDefaultPortExecution
 import hmeadowSocket.HMeadowSocket
 import settingsManager.SettingsManager
+import java.io.File
+import java.util.concurrent.TimeUnit
 
 fun main(args: Array<String>) {
     SettingsManager.settingsManager.saveSettings()
@@ -28,10 +28,26 @@ fun main(args: Array<String>) {
         is ListDestinationsCommand -> listDestinationsExecution(command = command)
         is DumpCommand -> dumpExecution(command = command)
         is ServerCommand -> serverExecution(command = command)
-        is SendFilesCommand -> sendFilesExecution(command = command)
+        is SendFilesCommand,
+        is SendMessageCommand,
+        -> {
+            val currentDirectory = System.getProperty("user.dir")
+            val clientEngineCmdLine = StringBuilder()
+                .append("java -jar $currentDirectory/build/compose/jars/FittoniaClientEngine-linux-x64-1.0.jar")
+            args.forEach {
+                clientEngineCmdLine.append(' ')
+                clientEngineCmdLine.append(it)
+            }
+            ProcessBuilder(*clientEngineCmdLine.toString().split(' ').toTypedArray())
+                .directory(File(currentDirectory))
+                .redirectOutput(ProcessBuilder.Redirect.INHERIT)
+                .redirectError(ProcessBuilder.Redirect.INHERIT)
+                .start()
+                .waitFor(60, TimeUnit.MINUTES)
+        }
+
         is SetDefaultPortCommand -> setDefaultPortExecution(command = command)
         is ServerPasswordCommand -> serverPasswordExecution(command = command)
-        is SendMessageCommand -> sendMessageExecution(command = command)
         else -> throw IllegalStateException("No valid command detected.")
     }
 
