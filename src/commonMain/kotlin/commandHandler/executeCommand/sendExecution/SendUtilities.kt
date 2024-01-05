af1ffcc4eb7c59c902527c4fbf996decb8b3578a
Hunter Wiesman <alphakand@gmail.com>
2024-01-04 17:02:24 -0700

    Simplify `startClientEngine` function a little.

--- src/commonMain/kotlin/commandHandler/executeCommand/sendExecution/SendUtilities.kt
+++ src/commonMain/kotlin/commandHandler/executeCommand/sendExecution/SendUtilities.kt
@@ -3,23 +3,19 @@ package commandHandler.executeCommand.sendExecution
 import java.io.File
 import java.util.concurrent.TimeUnit
 
-fun startClientEngine(inputTokens: List<String>) {
-    object : Thread() {
-        override fun run() {
-            val currentDirectory = System.getProperty("user.dir")
-            val clientEngineCmdLine = StringBuilder()
-                .append("java -jar $currentDirectory/build/compose/jars/FittoniaClientEngine-linux-x64-1.0.jar")
-            inputTokens.forEach {
-                clientEngineCmdLine.append(' ')
-                clientEngineCmdLine.append(it)
-            }
+fun startClientEngine(inputTokens: List<String>) = Thread {
+    val currentDirectory = System.getProperty("user.dir")
+    val clientEngineCmdLine = StringBuilder()
+        .append("java -jar $currentDirectory/build/compose/jars/FittoniaClientEngine-linux-x64-1.0.jar")
+    inputTokens.forEach {
+        clientEngineCmdLine.append(' ')
+        clientEngineCmdLine.append(it)
+    }
 
-            ProcessBuilder(*clientEngineCmdLine.toString().split(' ').toTypedArray())
-                .directory(File(currentDirectory))
-                .redirectOutput(ProcessBuilder.Redirect.INHERIT)
-                .redirectError(ProcessBuilder.Redirect.INHERIT)
-                .start()
-                .waitFor(60, TimeUnit.MINUTES)
-        }
-    }.start()
-}
+    ProcessBuilder(*clientEngineCmdLine.toString().split(' ').toTypedArray())
+        .directory(File(currentDirectory))
+        .redirectOutput(ProcessBuilder.Redirect.INHERIT)
+        .redirectError(ProcessBuilder.Redirect.INHERIT)
+        .start()
+        .waitFor(60, TimeUnit.MINUTES)
+}.start()
