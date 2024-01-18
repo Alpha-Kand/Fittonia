696ca344ab05b7b3f06c97f09cf28037e7a24329
Hunter Wiesman <alphakand@gmail.com>
2024-01-17 17:02:21 -0700

    Create global reference to Kotter Session.

--- src/commonMain/kotlin/Utility.kt
+++ src/commonMain/kotlin/Utility.kt
@@ -1,13 +1,18 @@
+import KotterSession.kotter
 import com.varabyte.kotter.foundation.text.Color
 import com.varabyte.kotter.foundation.text.color
 import com.varabyte.kotter.foundation.text.red
 import com.varabyte.kotter.foundation.text.rgb
 import com.varabyte.kotter.foundation.text.text
 import com.varabyte.kotter.foundation.text.textLine
+import com.varabyte.kotter.runtime.MainRenderScope
+import com.varabyte.kotter.runtime.RunScope
 import com.varabyte.kotter.runtime.Session
 import commandHandler.ServerFlags
 import hmeadowSocket.HMeadowSocket
 import hmeadowSocket.HMeadowSocketClient
+import kotlinx.coroutines.DelicateCoroutinesApi
+import kotlinx.coroutines.GlobalScope
 
 fun <T> requireNull(value: T?) {
     if (value != null) {
@@ -25,9 +30,9 @@ fun Session.reportHMSocketError(e: HMeadowSocket.HMeadowSocketError) = section {
     } ?: textLine(text = ".")
 }.run()
 
-fun Session.printLine(text: String, color: Color = Color.WHITE) = section { color(color); textLine(text) }.run()
-fun Session.printLine(text: String, color: Int) = section { rgb(color); textLine(text) }.run()
-fun Session.printLine() = section { textLine() }.run()
+fun printLine(text: String, color: Color = Color.WHITE) = kotter.section { color(color); textLine(text) }.run()
+fun printLine(text: String, color: Int) = kotter.section { rgb(color); textLine(text) }.run()
+fun printLine() = kotter.section { textLine() }.run()
 
 fun HMeadowSocketClient.reportTextLine(text: String, color: Color = Color.WHITE) {
     sendInt(message = ServerFlags.PRINT_LINE)
@@ -46,3 +51,16 @@ fun <T> HMeadowSocket.receiveApproval(onConfirm: () -> T, onDeny: () -> T): T {
 fun HMeadowSocket.sendConfirmation() = sendBoolean(true)
 
 fun HMeadowSocket.sendDeny() = sendBoolean(false)
+
+object KotterSession {
+    lateinit var kotter: Session
+}
+
+@OptIn(DelicateCoroutinesApi::class)
+fun kotterSection(renderBlock: MainRenderScope.() -> Unit, runBlock: RunScope.() -> Unit) {
+    if (Config.isMockking) {
+        runBlock(RunScope(kotter.section {}, GlobalScope))
+    } else {
+        kotter.section(renderBlock).run(runBlock)
+    }
+}
