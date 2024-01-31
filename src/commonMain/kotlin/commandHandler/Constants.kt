426a48607f49122ea5cb1c76ea800894f3b3c928
Hunter Wiesman <alphakand@gmail.com>
2024-01-31 12:29:04 -0700

    Add string constants in preparation to switch from transfering ints to strings (to help with debugging).

--- src/commonMain/kotlin/commandHandler/Constants.kt
+++ src/commonMain/kotlin/commandHandler/Constants.kt
@@ -66,17 +66,29 @@ class ServerFlagsString {
     companion object {
         const val CONFIRM = "CONFIRM"
         const val DENY = "DENY"
+
+        const val ADD_DESTINATION = "ADD_DESTINATION"
+
+        const val HAVE_JOB_NAME = "HAVE_JOB_NAME"
+        const val NEED_JOB_NAME = "NEED_JOB_NAME"
+
+        const val DONE = "DONE"
+        const val HAS_MORE = "HAS_MORE"
+        const val PRINT_LINE = "PRINT_LINE"
+        const val FILE_NAMES_TOO_LONG = "FILE_NAMES_TOO_LONG"
+        const val SEND_FILES_COLLECTING = "SEND_FILES_COLLECTING"
     }
 }
 
-enum class ServerCommandFlag {
-    SEND_FILES,
-    SEND_MESSAGE,
-    ADD_DESTINATION,
+enum class ServerCommandFlag(val text: String) {
+    SEND_FILES(text = "SEND_FILES"),
+    SEND_MESSAGE(text = "SEND_MESSAGE"),
+    ADD_DESTINATION(text = "ADD_DESTINATION"),
     ;
 
     companion object {
         fun Int.toCommandFlag() = entries[this]
+        fun String.toCommandFlag() = entries.find { it.text == this }
     }
 }
 
