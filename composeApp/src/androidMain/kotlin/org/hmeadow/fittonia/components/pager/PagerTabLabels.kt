f4f75991507fe45b25ab851ca5661c36659cde34
Hunter Wiesman <hunter.wiesman@neofinancial.com>
2025-03-06 12:48:17 -0700

    Replace old text with Material3 resource.

--- composeApp/src/androidMain/kotlin/org/hmeadow/fittonia/components/pager/PagerTabLabels.kt
+++ composeApp/src/androidMain/kotlin/org/hmeadow/fittonia/components/pager/PagerTabLabels.kt
@@ -14,7 +14,7 @@ import androidx.compose.foundation.relocation.BringIntoViewRequester
 import androidx.compose.foundation.relocation.bringIntoViewRequester
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.Text
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
