30710ff569a37adb3ba6682d6d369e03e4fd2b13
Hunter Wiesman <alphakand@gmail.com>
2025-06-03 17:36:10 -0600

    Update button font.

--- composeApp/src/commonMain/kotlin/org/hmeadow/fittonia/compose/components/FittoniaButtonConstants.kt
+++ composeApp/src/commonMain/kotlin/org/hmeadow/fittonia/compose/components/FittoniaButtonConstants.kt
@@ -1,18 +1,23 @@
 package org.hmeadow.fittonia.compose.components
 
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import org.hmeadow.fittonia.design.fonts.firaSansFont
 
 data object FittoniaButtonConstants {
     const val BORDER_WIDTH = 2
     const val BUTTON_ICON_SIZE = 17
     const val CORNER_RADIUS = 5
 
-    val buttonTextStyle = TextStyle(
-        fontSize = 17.sp,
-        lineHeight = 23.sp,
-        letterSpacing = (0.08f).sp,
-        fontWeight = FontWeight(weight = 600),
-    )
+    val buttonTextStyle: TextStyle
+        @Composable
+        get() = TextStyle(
+            fontSize = 17.sp,
+            lineHeight = 23.sp,
+            letterSpacing = (0.08f).sp,
+            fontWeight = FontWeight(weight = 600),
+            fontFamily = firaSansFont,
+        )
 }
