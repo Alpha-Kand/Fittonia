558b66e314b7bf69b43431d2f0cd89f1ad984bbd
Hunter Wiesman <alphakand@gmail.com>
2025-08-17 13:24:17 -0600

    Move OverviewScreenViewModel to its own file.

--- composeApp/src/androidMain/kotlin/org/hmeadow/fittonia/screens/overviewScreen/OverviewScreen.kt
+++ composeApp/src/androidMain/kotlin/org/hmeadow/fittonia/screens/overviewScreen/OverviewScreen.kt
@@ -1,7 +1,5 @@
 package org.hmeadow.fittonia.screens.overviewScreen
 
-import SettingsManager
-import android.net.Uri
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.BoxWithConstraints
 import androidx.compose.foundation.layout.Column
@@ -24,15 +22,11 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
-import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.update
-import kotlinx.coroutines.launch
-import org.hmeadow.fittonia.BaseViewModel
 import org.hmeadow.fittonia.MainActivity
 import org.hmeadow.fittonia.R
 import org.hmeadow.fittonia.SettingsDataAndroid
 import org.hmeadow.fittonia.UserAlert
-import org.hmeadow.fittonia.androidServer.AndroidServer
 import org.hmeadow.fittonia.components.FittoniaHeader
 import org.hmeadow.fittonia.components.FittoniaIcon
 import org.hmeadow.fittonia.components.FittoniaModal
@@ -52,73 +46,17 @@ import org.hmeadow.fittonia.design.Spacing.spacing16
 import org.hmeadow.fittonia.design.Spacing.spacing8
 import org.hmeadow.fittonia.design.fonts.headingMStyle
 import org.hmeadow.fittonia.design.fonts.paragraphTextStyle
-import org.hmeadow.fittonia.models.OutgoingJob
 import org.hmeadow.fittonia.models.TransferJob
-import org.hmeadow.fittonia.models.TransferStatus
-import org.hmeadow.fittonia.utility.createJobDirectory
 import org.hmeadow.fittonia.utility.isLandscape
-import kotlin.math.abs
-import kotlin.random.Random
 
+// TODO move this somewhere better.
 class Options(
     val name: String,
     val onClick: () -> Unit,
 )
 
-class OverviewScreenViewModel(
-    private val onUpdateDumpPath: (Uri) -> Unit,
-) : BaseViewModel() {
-    val needDumpAccess = MutableStateFlow(false)
-
-    init {
-        launch {
-            when (val state = MainActivity.mainActivity.createJobDirectory(jobName = "ACCESS")) {
-                is MainActivity.CreateDumpDirectory.Error -> {
-                    needDumpAccess.update { true }
-                    MainActivity.mainActivity.alert(UserAlert.DumpLocationLost)
-                }
-
-                is MainActivity.CreateDumpDirectory.Success -> MainActivity.mainActivity.deleteDumpDirectory(state.uri)
-            }
-        }
-    }
-
-    fun onDumpPathPicked(path: Uri) {
-        onUpdateDumpPath(path)
-        needDumpAccess.update { false }
-        MainActivity.mainActivity.unAlert<UserAlert.DumpLocationLost>()
-    }
-
-    fun addNewDebugJob() = launch {
-        AndroidServer.server.value?.registerTransferJob(
-            OutgoingJob(
-                id = Random.nextInt(),
-                description = "Sending PDFs to bob (${abs(Random.nextInt() % 100)})",
-                needDescription = false,
-                destination = SettingsManager.Destination(
-                    name = "Bob's PC (${abs(Random.nextInt() % 100)})",
-                    ip = "192.168.1.1",
-                    accessCode = "accesscode",
-                ),
-                items = (0..abs(Random.nextInt() % 20)).map {
-                    val totalSize = abs(Random.nextLong())
-                    TransferJob.Item(
-                        name = "File_${abs(Random.nextInt() % 100)}.pdf",
-                        uriRaw = "https://www.google.com",
-                        isFile = true,
-                        sizeBytes = totalSize,
-                        progressBytes = Random.nextLong(from = 0, until = totalSize),
-                    )
-                },
-                port = 5556,
-                status = TransferStatus.entries.random(),
-            ),
-        )
-    }
-}
-
 @Composable
-fun OverviewScreen(
+internal fun OverviewScreen(
     viewModel: OverviewScreenViewModel,
     data: SettingsDataAndroid,
     onSendFilesClicked: () -> Unit,
