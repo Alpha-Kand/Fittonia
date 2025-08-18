2e8577dd2c868661b46dfb74c7e61ecc79933fec
Hunter Wiesman <alphakand@gmail.com>
2025-08-18 13:05:13 -0600

    Improve ip encode/decode to be easier to remember and type.

--- composeApp/src/commonMain/kotlin/org/hmeadow/fittonia/utility/EncodeDecodeIpAddress.kt
+++ composeApp/src/commonMain/kotlin/org/hmeadow/fittonia/utility/EncodeDecodeIpAddress.kt
@@ -14,7 +14,7 @@ fun encodeIpAddress(ipAddress: String): String {
         val firstWord = StringBuilder().append(hexes[0][0], hexes[1][0], hexes[2][0]).toString()
         val secondWord = StringBuilder().append(hexes[0][1], hexes[1][1], hexes[2][1]).toString()
         val getWord: (String) -> String = { wordList[it.toInt(radix = 16)] }
-        return "${getWord(firstWord)}-${getWord(secondWord)}-${hexes.last().toInt(radix = 16)}"
+        return "${getWord(firstWord)}.${getWord(secondWord)}.${hexes.last().toInt(radix = 16)}"
     } catch (_: Exception) {
         throw IllegalArgumentException(
             "ipAddress \"$ipAddress\" was not in expected format: \"X.X.X.X\" where (0 <= X <= 255)",
@@ -24,13 +24,13 @@ fun encodeIpAddress(ipAddress: String): String {
 
 fun decodeIpAddress(ipAddress: String): String {
     try {
-        val tokens = ipAddress.lowercase().split('-').also { require(it.size == 3) }
+        val tokens = ipAddress.lowercase().split('.').also { require(it.size == 3) }
         val encryptedHex1 = wordList.indexOf(tokens[0]).toString(radix = 16).padStart(length = 3, padChar = '0')
         val encryptedHex2 = wordList.indexOf(tokens[1]).toString(radix = 16).padStart(length = 3, padChar = '0')
         val addressChunk1 = "${encryptedHex1[0]}${encryptedHex2[0]}".toInt(radix = 16)
         val addressChunk2 = "${encryptedHex1[1]}${encryptedHex2[1]}".toInt(radix = 16)
         val addressChunk3 = "${encryptedHex1[2]}${encryptedHex2[2]}".toInt(radix = 16)
-        val addressChunk4 = require(tokens[2].toInt() in (0..255))
+        val addressChunk4: Int = tokens[2].toInt().also { require(it in (0..255)) }
         return "$addressChunk1.$addressChunk2.$addressChunk3.$addressChunk4"
     } catch (_: Exception) {
         throw IllegalArgumentException(
